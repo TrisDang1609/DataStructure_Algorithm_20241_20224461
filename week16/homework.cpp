93bd8ebd6560a48d8db3d8b3024a891d89b7ba82
TrisDang1609 <Tri.HM224461@sis.hust.edu.vn>
2024-12-24 23:47:11 +0700

    update util 12-24-2024

---
+++ week16/homework.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+};
+
+int getHeight(Node* node) {
+    return node ? node->height : 0;
+}
+
+int getBalanceFactor(Node* node) {
+    return node ? getHeight(node->left) - getHeight(node->right) : 0;
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+Node* createNode(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = node->right = nullptr;
+    node->height = 1;
+    return node;
+}
+
+Node* rotateRight(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    return x;
+}
+
+Node* rotateLeft(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    return y;
+}
+
+Node* insert(Node* node, int key) {
+    if (!node) return createNode(key);
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+    int balance = getBalanceFactor(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rotateRight(node);
+    if (balance < -1 && key > node->right->key)
+        return rotateLeft(node);
+    if (balance > 1 && key > node->left->key) {
+        node->left = rotateLeft(node->left);
+        return rotateRight(node);
+    }
+    if (balance < -1 && key < node->right->key) {
+        node->right = rotateRight(node->right);
+        return rotateLeft(node);
+    }
+
+    return node;
+}
+
+void preOrder(Node* root) {
+    if (root) {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+    int arr[] = {10, 20, 30, 40, 50, 25};
+    for (int key : arr)
+        root = insert(root, key);
+
+    preOrder(root);
+    return 0;
+}
